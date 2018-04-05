97f414fad8c40daee398bda9d62eed516ecd9370
mikhalev <mikhalev@rp.team>
2018-04-05 22:31:31 +0300

    slide menu works fine

--- SlideMENU/ViewController.swift
+++ SlideMENU/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SlideMenu: NSLayoutConstraint!
+    
+    var isMenuHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SlideMenu.constant = -240 // hide
     }
 
+    @IBAction func MenuButtonPressed(_ sender: UIBarButtonItem) {
+        if isMenuHidden {
+            SlideMenu.constant = 0
+            UIView.animate(withDuration: 0.4, animations: { self.view.layoutIfNeeded() })
+        } else {
+            SlideMenu.constant = -240
+            UIView.animate(withDuration: 0.4, animations: { self.view.layoutIfNeeded() })
+        }
+        isMenuHidden = !isMenuHidden // invert
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
